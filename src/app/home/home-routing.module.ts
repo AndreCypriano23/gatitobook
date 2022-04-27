f74a2cf9901e1e92e089878e360a89ce32effb78
AndreCypriano23 <andrecypriano23@outlook.com>
2022-04-26 22:25:54 -0300

    Modulos organizados e configurados, assim como as primeiras rotas

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent, //quando for uma rota vazia, ele vai projetar o Login componente
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
