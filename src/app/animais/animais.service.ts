c3fdb70dc8c323fe5b7ec85c1731d35ce1c17d75
AndreCypriano23 <andrecypriano23@outlook.com>
2022-08-15 14:19:31 -0300

    Detalhes do animal desenvolvido

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mapTo } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 //Importante: não utilize essa constante entre o decorator Injectable
 //e a classe, serão dará erro de compilação
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -21,14 +23,25 @@ export class AnimaisService {
 
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais>{
-      //mais um detalhe: preciso enviar de alguma forma o Token de autenticalção, no Header
-      //da requisição pq a nossa API exige um Token
-      const token = this.tokenService.retornaToken();
-      const headers = new HttpHeaders().append('x-access-token', token);
       //vai retornar um observable, que é um retorno da minha API
-      return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-        headers,
-      });
+      return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
+  buscaPorId(id: number): Observable<Animal>{
+    return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluiAnimal(id: number): Observable<Animal>{
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  //Na API ela retorna 200 caso OK, ou 304 caso o curtir com o mesmo Token já tenha sido feito
+  curtir(id: number): Observable<boolean>{
+    return this.http.post(`${API}/photos/${id}/like`, {},
+     { observe: 'response' } //o Angular vai passar a response inteira
+    ).pipe(//vou manipular os fluxos da requisição.
+       mapTo(true), catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+      }));
+  }
 }
